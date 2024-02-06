a85941faea2692589bcdc7f8e634f29e4b311c02
Manuel Kea Baldassarri <kea@keasoft.it>
2024-02-06 18:26:52 +0100

    Add min version certbot require

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ test_extras = [
     "pytest",
 ]
 
+install_requires = []
+
+install_requires.extend([
+    'certbot>=1.21',
+])
+
 setup(
     packages=find_packages(),
-    install_requires=[
-        "certbot",
-    ],
+    install_requires=install_requires,
     extras_require={
         "test": test_extras,
     },

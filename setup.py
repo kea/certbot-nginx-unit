60edf265f6cdb7c65266340efe9589ae622067b7
Manuel Baldassarri <kea@keasoft.it>
2024-12-21 15:31:42 +0100

    Upgrade to snapcraft core24 and certbot >=2.12 (#3)

--- setup.py
+++ setup.py
@@ -7,9 +7,11 @@ test_extras = [
     "pytest",
 ]
 
+certbot_version = '2.12.0.dev0'
+
 install_requires = []
 if not os.environ.get("SNAP_BUILD"):
-    install_requires.extend(["acme>=1.21", "certbot>=1.21"])
+    install_requires.extend([f'acme>={certbot_version}', f'certbot>={certbot_version}'])
 else:
     install_requires.append("packaging")
 
